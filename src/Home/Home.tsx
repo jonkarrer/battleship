ee9d87c7da4f0f48331c60dc7511b8b571573520
jonkarrer <karrerjon@gmail.com>
2021-02-18 09:06:58 -0600

    homepage style applied

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './Home.css'
 
  const Home: React.FC = () => {
   return (
@@ -7,7 +7,7 @@ import React from 'react'
       <section className="home-menu-grid">
         <div className="new-game-cell"><h2>New Game</h2></div>
         <div className="rules-cell"><h2>Rules</h2></div>
-        <div className="battleship-cell"><h1>BattleShip</h1></div>
+        <div className="battleship-cell"><h1>BATTLESHIP</h1></div>
       </section>
       <div className="boat-1-homepage"><img src="./assets/1.jpg" alt="boat"/></div>
       <div className="boat-2-homepage"><img src="./assets/2.jpg" alt="boat"/></div>
