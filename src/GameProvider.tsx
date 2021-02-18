6d38792669aea940da1155872efa1b5523964711
jonkarrer <karrerjon@gmail.com>
2021-02-18 17:06:34 -0600

    update Read.md

--- src/GameProvider.tsx
+++ src/GameProvider.tsx
@@ -9,6 +9,7 @@ export const changeGameLevel = () => useContext(ChangeGameContext);
 interface GameProps {
   children: any;
 }
+
 export const GameProvider = ({children}:GameProps) => {
   const [gameLevel, setGameLevel] = useState(0);
   const changeLevel = () => setGameLevel(gameLevel + 1);
